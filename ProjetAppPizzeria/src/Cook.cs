579afed6584f3a57a290113be4d68086a73ffd9e
Gyoomu <60664665+Gyoomu@users.noreply.github.com>
2022-10-17 21:22:25 +0200

    add orders queue in cook

--- ProjetAppPizzeria/src/Cook.cs
+++ ProjetAppPizzeria/src/Cook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace ProjetAppPizzeria.src
     internal class Cook : Person
     {
         private int salary;
-
+        private ArrayList ordersQueue;
         public Cook(string firstName, string lastName, string phoneNumber, string streetNumber, string streetName, string city, string postalCode, string country, int salary) : base(firstName, lastName, phoneNumber, streetNumber, streetName, city, postalCode, country)
         {
             this.salary = salary;
@@ -18,7 +19,12 @@ namespace ProjetAppPizzeria.src
         {
             return this.salary;
         }
-        
+
+        public void addOrderQueue(Order order)
+        {
+            this.ordersQueue.Add(order);
+        }
+
         public override string ToString()
         {
             return base.ToString() + " Salary: " + this.salary;
